ec6a630cd66dc085209d8c657ad63ed3c64a15ef
adipa <adipa.achintha@gmail.com>
2016-03-12 15:16:57 +1100

    added IdentityServer3 sample

---
+++ OwinIdentityServer3/Web/Controllers/AccountController.cs
@@ -0,0 +1,52 @@
+﻿using System.Web.Http;
+using Domain.Interfaces.Repositories;
+using Domain.Models;
+using Domain.Models.Auth;
+
+namespace Web.Controllers
+{
+    [RoutePrefix("api/Account")]
+    public class AccountController : ApiController
+    {
+        private readonly IUserRepository _userRepository;
+
+        public AccountController(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        // POST api/Account/Register
+        [AllowAnonymous]
+        [Route("Register")]
+        public IHttpActionResult Register(UserModel userModel)
+        {
+             if (!ModelState.IsValid)
+             {
+                return BadRequest(ModelState);
+             }
+
+             var result = _userRepository.RegisterUser(userModel);
+             if (result == null)
+             {
+                 return InternalServerError();
+             }
+
+             return Ok();
+        }
+
+        [HttpPost]
+        [Route("list")]
+        public IHttpActionResult List(ListRequest request)
+        {
+            return Ok(_userRepository.List(request));
+        }
+
+        [HttpDelete]
+        [Route("{userName}")]
+        public IHttpActionResult Delete(string userName)
+        {
+            _userRepository.Delete(userName);
+            return Ok();
+        }
+    }
+}
