4d2cb5c87a26858c53f1421402fd516f63f91c68
adipa <adipa.achintha@gmail.com>
2016-03-12 15:16:57 +1100

    added IdentityServer3 sample

---
+++ OwinIdentityServer3/Web/OAuth/IIdentiyServiceFactoryProvider.cs
@@ -0,0 +1,9 @@
+﻿using IdentityServer3.Core.Configuration;
+
+namespace Web.OAuth
+{
+    public interface IIdentiyServiceFactoryProvider
+    {
+        IdentityServerServiceFactory CreateFactory();
+    }
+}
---
+++ OwinOAuth/Infrastructure/Plumbing/SQLStatementInterceptor.cs
@@ -0,0 +1,35 @@
+﻿using Domain.Interfaces.Plumbing;
+using Domain.Interfaces.Repositories;
+using NHibernate;
+using NHibernate.SqlCommand;
+using Ninject;
+
+namespace Infrastructure.Plumbing
+{
+    public class SQLStatementInterceptor : EmptyInterceptor , ISQLStatementInterceptor
+    {
+        private readonly IKernel _kernel;
+
+        public SQLStatementInterceptor(IKernel kernel)
+        {
+            _kernel = kernel;
+        }
+
+        public override SqlString OnPrepareStatement(SqlString sql)
+        {
+            var sqlString = sql.ToString();
+            var lower = sqlString.ToLower();
+            if (lower.Contains("logmessagerecord")
+                || lower.Contains("loghttprecord")
+                || lower.Contains("configsetting"))
+            {
+                return sql;
+            }
+
+            var repo = _kernel.Get<ILogWriterRepository>();
+            repo.LogSQL(sqlString);
+
+            return sql;
+        }
+    }
+}
