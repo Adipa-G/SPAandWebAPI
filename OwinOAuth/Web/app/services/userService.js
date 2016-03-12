ad437036a1641437230d26b0d3d310cb9088e773
adipa <adipa.achintha@gmail.com>
2016-03-12 15:16:57 +1100

    added IdentityServer3 sample

---
+++ OwinOAuth/Web/app/services/userService.js
@@ -0,0 +1,27 @@
+﻿'use strict';
+
+(function () {
+    var userServiceFactory = function ($http, $log, ngAuthSettings) {
+        var serviceBase = ngAuthSettings.apiServiceBaseUri;
+
+        var getUsers = function (sortAndPage) {
+            return $http.post(serviceBase + 'api/Account/list', sortAndPage).catch(function (err) {
+                $log.error(err);
+            });
+        };
+
+        var deleteUser = function (userName) {
+            return $http.delete(serviceBase + 'api/Account/' + userName).catch(function (err) {
+                $log.error(err);
+            });
+        };
+
+        return {
+            getUsers: getUsers,
+            deleteUser: deleteUser
+        }
+    };
+
+    var app = angular.module('AngularAuthApp');
+    app.factory('userService', ['$http','$log', 'ngAuthSettings', userServiceFactory]);
+})();
