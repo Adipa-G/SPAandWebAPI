146310748fa0517d5f7151508eb775068538e5af
adipa <adipa.achintha@gmail.com>
2016-03-22 23:12:47 +1100

    added mvc6 solution

---
+++ MVC6/src/Web/wwwroot/app/directives/jsonFormatHeighlight.js
@@ -0,0 +1,24 @@
+﻿'use strict';
+
+(function () {
+    var app = angular.module('AngularAuthApp');
+    app.directive('jsonFormatHeighlight', function () {
+        return {
+            restrict: 'A',
+            link: function (scope, elem, attrs) {
+                attrs.$observe('jsonFormatHeighlight', function (value) {
+                    if (!value)
+                        return;
+                    try {
+                        var formatted = vkbeautify.json(value);
+                        var highlighted = hljs.highlight('JSON', formatted);
+                        elem.html(highlighted.value);    
+                    }
+                    catch(e) {
+                        elem.html(value);
+                    }
+                });
+            }
+        };
+    });
+})();
