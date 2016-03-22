86e5e897c16ff772d983671a73e7217bbf24ea6d
adipa <adipa.achintha@gmail.com>
2016-03-22 23:12:47 +1100

    added mvc6 solution

---
+++ MVC6/src/Infrastructure/Repositories/LogViewRepository.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Domain;
+using Domain.Entities;
+using Domain.Enum;
+using Domain.Interfaces.Repositories;
+using Domain.Models;
+using Domain.Models.Log;
+using NHibernate;
+using NHibernate.Criterion;
+
+namespace Infrastructure.Repositories
+{
+    public class LogViewRepository : ILogViewRepository
+    {
+        private ISession _session;
+
+        public LogViewRepository(ISession session)
+        {
+            _session = session;
+        }
+
+        public List<string> GetAllLevels()
+        {
+            return new List<string>(Enum.GetNames(typeof (LogLevel)));
+        }
+
+        public List<string> GetAllLoggers()
+        {
+            return new List<string>(Enum.GetNames(typeof(LoggerName)));
+        }
+
+        public ListResult<LogMessageListItemModel> GetLogMessages(LogMessageListRequest request)
+        {
+            var query = _session.QueryOver<LogMessageRecord>();
+            if (request.LogLevel.HasValue)
+            {
+                query = query.Where(lm => lm.Level == request.LogLevel);
+            }
+            if (!string.IsNullOrEmpty(request.Logger))
+            {
+                query = query.Where(lm => lm.Logger == request.Logger);
+            }
+            if (!string.IsNullOrWhiteSpace(request.FromDate))
+            {
+                query = query.Where(lm => lm.LogTimestamp > request.FromDate.Timestamp());
+            }
+            if (!string.IsNullOrWhiteSpace(request.ToDate))
+            {
+                query = query.Where(lm => lm.LogTimestamp < request.ToDate.Timestamp());
+            }
+            if (request.OrderDirection == SortDirection.Asc)
+            {
+                query = query.OrderBy(Projections.Property(request.OrderField)).Asc;
+            }
+            else if (request.OrderDirection == SortDirection.Desc)
+            {
+                query = query.OrderBy(Projections.Property(request.OrderField)).Desc;
+            }
+
+            var totalCount = query.RowCount();
+            var results =
+                query.Skip((request.PageNumber - 1) * request.PageSize)
+                    .Take(request.PageSize)
+                    .List()
+                    .Select(
+                        r =>
+                            new LogMessageListItemModel()
+                            {
+                                Id = r.Id,
+                                LogTimestamp = r.LogTimestamp.Timestamp(),
+                                Message = r.Message,
+                                Level = Enum.GetName(typeof (LogLevel), r.Level),
+                                Logger = r.Logger,
+                                StackTrace = r.StackTrace
+                            })
+                    .ToList();
+
+            return new ListResult<LogMessageListItemModel>()
+            {
+                PageNumber = request.PageNumber,
+                PageSize = request.PageSize,
+                Results = results,
+                TotalCount = totalCount
+            };
+        }
+
+        public ListResult<LogHttpListItemModel> GetLogHttp(LogHttpListRequest request)
+        {
+            var query = _session.QueryOver<LogHttpRecord>();
+            if (!string.IsNullOrEmpty(request.TrackingId))
+            {
+                query = query.Where(lm => lm.TrackingId == request.TrackingId);
+            }
+            else
+            {
+                if (request.LogLevel.HasValue)
+                {
+                    query = query.Where(lm => lm.Level == request.LogLevel);
+                }
+                if (!string.IsNullOrWhiteSpace(request.Identity))
+                {
+                    query = query.Where(lm => lm.RequestIdentity == request.Identity);
+                }
+                if (!string.IsNullOrWhiteSpace(request.FromDate))
+                {
+                    query = query.Where(lm => lm.CalledOn > request.FromDate.Timestamp());
+                }
+                if (!string.IsNullOrWhiteSpace(request.ToDate))
+                {
+                    query = query.Where(lm => lm.CalledOn < request.ToDate.Timestamp());
+                }
+                if (request.OrderDirection == SortDirection.Asc)
+                {
+                    query = query.OrderBy(Projections.Property(request.OrderField)).Asc;
+                }
+                else if (request.OrderDirection == SortDirection.Desc)
+                {
+                    query = query.OrderBy(Projections.Property(request.OrderField)).Desc;
+                }
+            }
+
+            var totalCount = query.RowCount();
+            var results =
+                query.Skip((request.PageNumber - 1) * request.PageSize)
+                    .Take(request.PageSize)
+                    .List()
+                    .Select(
+                        r =>
+                            new LogHttpListItemModel()
+                            {
+                                Id = r.Id,
+                                LogTimestamp = r.CalledOn.Timestamp(),
+                                TrackingId = r.TrackingId,
+                                Caller = r.RequestIdentity,
+                                Verb = r.Verb,
+                                RequestUri = r.RequestUri,
+                                RequestHeaders = r.RequestHeaders,
+                                Request = r.Request.Length > 1000 ? r.Request.Substring(0, 1000) : r.Request,
+                                Status = string.Format("{0}\n{1}", r.StatusCode, r.ReasonPhrase),
+                                ResponseHeaders = r.ResponseHeaders,
+                                Response = r.Response.Length > 1000 ? r.Response.Substring(0,1000) : r.Response,
+                                Duration = r.CallDuration.TotalSeconds.ToString()
+                            })
+                    .ToList();
+
+            return new ListResult<LogHttpListItemModel>()
+            {
+                PageNumber = request.PageNumber,
+                PageSize = request.PageSize,
+                Results = results,
+                TotalCount = totalCount
+            };
+        }
+    }
+}
---
+++ MVC6/src/Web/Startup.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.PlatformAbstractions;
+using System.Security.Cryptography.X509Certificates;
+using System.IO;
+using Infrastructure.Modules;
+using Microsoft.AspNet.Mvc.Formatters;
+using Newtonsoft.Json.Serialization;
+using Web.Modules;
+
+namespace Web
+{
+    public class Startup
+    {
+        private readonly IApplicationEnvironment _environment;
+
+        public Startup(IApplicationEnvironment environment)
+        {
+            _environment = environment;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            PlumbingModule.Load(services);
+            RepositoryModule.Load(services);
+            WebModule.Load(services);
+
+            var cert =
+                new X509Certificate2(Path.Combine(_environment.ApplicationBasePath, "Configuration", "idsrv4test.pfx"),
+                    "idsrv3test");
+
+            services.AddIdentityServer(options =>
+                                       {
+                                           options.SigningCertificate = cert;
+                                       });
+
+            services.AddMvc(options =>
+                            {
+
+                                var jsonOutputFormatter = new JsonOutputFormatter();
+                                jsonOutputFormatter.SerializerSettings.ContractResolver =
+                                    new CamelCasePropertyNamesContractResolver();
+                                options.OutputFormatters.Insert(0, jsonOutputFormatter);
+                            });
+        }
+
+        public void Configure(IApplicationBuilder app, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole(LogLevel.Verbose);
+            loggerFactory.AddDebug(LogLevel.Verbose);
+
+            app.UseDeveloperExceptionPage();
+            app.UseIISPlatformHandler();
+
+            app.UseIdentityServer();
+
+            app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();
+        }
+
+        public static void Main(string[] args) => WebApplication.Run<Startup>(args);
+    }
+}
