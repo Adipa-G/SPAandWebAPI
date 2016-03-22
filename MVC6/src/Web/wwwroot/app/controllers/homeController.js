6e5687f6a6fa8d4b42ff9d9595e7c9e1e2dcd3d5
adipa <adipa.achintha@gmail.com>
2016-03-22 23:12:47 +1100

    added mvc6 solution

---
+++ MVC6/src/Web/wwwroot/app/controllers/homeController.js
@@ -0,0 +1,10 @@
+﻿'use strict';
+
+(function () {
+    var homeController = function() {
+
+    };
+
+    var app = angular.module('AngularAuthApp');
+    app.controller('homeController', [homeController]);
+})();
