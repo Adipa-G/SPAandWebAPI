8face0f19844c9aeccb9cc7e259b6c681295616e
adipa <adipa.achintha@gmail.com>
2016-04-12 00:13:39 +1000

    start migrating to angular2

---
+++ Angular2/src-test/Web.Test/Controllers/LogControllerTest.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections.Generic;
+using System.Net;
+using Domain.Interfaces.Repositories;
+using Domain.Models;
+using Domain.Models.Log;
+using Microsoft.AspNet.Mvc;
+using NSubstitute;
+using NUnit.Framework;
+using Web.Controllers;
+
+namespace Web.Test.Controllers
+{
+    [TestFixture]
+    public class LogControllerTest
+    {
+        private ILogViewRepository _logViewRepository;
+
+        private LogController _controller;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _logViewRepository = Substitute.For<ILogViewRepository>();
+            _controller = new LogController(_logViewRepository);
+        }
+
+        [Test]
+        public void GivenLevels_WhenLevels_ThenReturn()
+        {
+            _logViewRepository.GetAllLevels().Returns(new List<string>() {"Info", "Error"});
+
+            var result = _controller.Levels() as HttpOkObjectResult;
+
+            Assert.AreEqual(HttpStatusCode.OK, (HttpStatusCode)result.StatusCode);
+            _logViewRepository.Received(1).GetAllLevels();
+        }
+        
+        [Test]
+        public void GivenLoggers_WhenLoggers_ThenReturn()
+        {
+            _logViewRepository.GetAllLoggers().Returns(new List<string>() { "General", "SQL" });
+
+            var result = _controller.Loggers() as HttpOkObjectResult; 
+
+            Assert.AreEqual(HttpStatusCode.OK, (HttpStatusCode)result.StatusCode);
+            _logViewRepository.Received(1).GetAllLoggers();
+        }
+
+        [Test]
+        public void GivenLogMessages_WhenLogMessages_ThenReturn()
+        {
+            _logViewRepository.GetLogMessages(Arg.Any<LogMessageListRequest>()).Returns(new ListResult<LogMessageListItemModel>());
+
+            var result = _controller.LogMessages(new LogMessageListRequest()) as HttpOkObjectResult; 
+
+            Assert.AreEqual(HttpStatusCode.OK, (HttpStatusCode)result.StatusCode);
+            _logViewRepository.Received(1).GetLogMessages(Arg.Any<LogMessageListRequest>());
+        }
+
+        [Test]
+        public void GivenLogHttp_WhenLogHttp_ThenReturn()
+        {
+            _logViewRepository.GetLogHttp(Arg.Any<LogHttpListRequest>()).Returns(new ListResult<LogHttpListItemModel>());
+
+            var result = _controller.LogHttp(new LogHttpListRequest()) as HttpOkObjectResult; 
+
+            Assert.AreEqual(HttpStatusCode.OK, (HttpStatusCode)result.StatusCode);
+            _logViewRepository.Received(1).GetLogHttp(Arg.Any<LogHttpListRequest>());
+        }
+    }
+}
---
+++ Angular2/src-test/Web.Test/app/directives/sortHeaderTest.js
@@ -0,0 +1,61 @@
+﻿'use strict';
+
+describe('sort header test', function () {
+    var $compile;
+    var $rootScope;
+
+    beforeEach(module('AngularAuthApp'));
+
+    beforeEach(inject(function (_$compile_, _$rootScope_) {
+        $compile = _$compile_;
+        $rootScope = _$rootScope_;
+    }));
+
+    it('Set the icon for ascending', function () {
+        var element = $compile("<th sort-header='sortData' sort-field='UserName'>Username</th>")($rootScope);
+        $rootScope.$digest();
+
+        var scope = element.scope();
+        scope.sortData = { OrderField: 'UserName', OrderDirection: 'Asc' };
+        scope.$digest();
+
+        expect(element.html()).toContain("fa-sort-alpha-asc");
+    });
+
+    it('Set the icon for descending', function () {
+        var element = $compile("<th sort-header='sortData' sort-field='UserName'>Username</th>")($rootScope);
+        $rootScope.$digest();
+
+        var scope = element.scope();
+        scope.sortData = { OrderField: 'UserName', OrderDirection: 'Desc' };
+        scope.$digest();
+
+        expect(element.html()).toContain("fa-sort-alpha-desc");
+    });
+
+    it('click switch icon and call onChange funtion', function () {
+        var element = $compile("<th sort-header='sortData' sort-field='UserName'>Username</th>")($rootScope);
+        $rootScope.$digest();
+
+        var changed = false;
+        
+        var scope = element.scope();
+        scope.sortData = { OrderField: 'UserName', OrderDirection: 'None', onChange : function () { changed = true; } };
+        scope.$digest();
+
+        element.triggerHandler('click');
+
+        expect(element.html()).toContain("fa-sort-alpha-asc");
+        expect(scope.sortData.OrderDirection).toBe('Asc');
+        expect(changed).toBe(true);
+
+        element.triggerHandler('click');
+
+        expect(element.html()).toContain("fa-sort-alpha-desc");
+        expect(scope.sortData.OrderDirection).toBe('Desc');
+
+        element.triggerHandler('click');
+
+        expect(scope.sortData.OrderDirection).toBe('None');
+    });
+});
