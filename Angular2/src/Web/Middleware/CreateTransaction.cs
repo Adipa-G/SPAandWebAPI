7bc631c8245e807a4f0d40cb7102f501ec99ab23
adipa <adipa.achintha@gmail.com>
2016-04-12 00:13:39 +1000

    start migrating to angular2

---
+++ Angular2/src/Web/Middleware/CreateTransaction.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Http;
+using Microsoft.Extensions.DependencyInjection;
+using NHibernate;
+
+namespace Web.Middleware
+{
+    public class CreateTransaction
+    {
+        private readonly RequestDelegate _next;
+
+        public CreateTransaction(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            var session = context.ApplicationServices.GetService<ISession>();
+
+            var needTransaction = new[] { "POST", "PUT", "DELETE" }.Contains(context.Request.Method);
+            if (needTransaction)
+            {
+                using (var transaction = session.BeginTransaction())
+                {
+                    try
+                    {
+                        await _next.Invoke(context);
+                        transaction.Commit();
+                    }
+                    catch (Exception)
+                    {
+                        transaction.Rollback();
+                        throw;
+                    }
+                }
+            }
+            else
+            {
+                await _next.Invoke(context);
+            }
+        }
+    }
+}
---
+++ Angular2/src/Web/scripts/app.ts
@@ -0,0 +1,9 @@
+﻿import {Component} from 'angular2/core';
+import {MenuComponent} from './shared/components/MenuComponent';
+
+@Component({
+    selector: 'angular-auth-app',
+    template: '<shared-menu></shared-menu>',
+    directives: [MenuComponent]
+})
+export class AppComponent { }
