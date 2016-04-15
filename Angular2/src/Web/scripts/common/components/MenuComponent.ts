a96f738b3fb81a8b313eeb94178e956549f52602
adipa <adipa.achintha@gmail.com>
2016-04-16 01:14:13 +1000

    added login component and still not finished auth mechanism

--- Angular2/src/Web/scripts/common/components/MenuComponent.ts
+++ Angular2/src/Web/scripts/common/components/MenuComponent.ts
@@ -1,10 +1,13 @@
 ﻿import {Component} from 'angular2/core';
+import { ROUTER_DIRECTIVES } from 'angular2/router'
+
 import {AuthenticationInfo} from '../../domain/auth/AuthenticationInfo';
 import {AuthService} from "../services/AuthService";
 
 @Component({
     selector: 'common-menu',
     viewProviders: [AuthService],
+    directives: [ROUTER_DIRECTIVES],
     templateUrl: './templates/common/components/MenuComponent.html'
 })
 
