0e8f5055b1f1fe57d121a09bb328c047052fa699
adipa <adipa.achintha@gmail.com>
2016-04-19 23:22:52 +1000

    added events to handle login and logout events

--- Angular2/src/Web/wwwroot/appScripts/boot.js
+++ Angular2/src/Web/wwwroot/appScripts/boot.js
@@ -1,9 +1,11 @@
 ///<reference path="../node_modules/angular2/typings/browser.d.ts"/>
 var browser_1 = require('angular2/platform/browser');
 var http_1 = require('angular2/http');
-var LogService_1 = require('./common/services/LogService');
 var Constants_1 = require('./common/services/Constants');
+var LogService_1 = require('./common/services/LogService');
+var ErrorService_1 = require('./common/services/ErrorService');
 var StorageService_1 = require('./common/services/StorageService');
+var AuthService_1 = require('./common/services/AuthService');
 var HttpClient_1 = require('./common/services/HttpClient');
 var app_1 = require('./app');
 browser_1.bootstrap(app_1.AppComponent, [
@@ -11,7 +13,9 @@ browser_1.bootstrap(app_1.AppComponent, [
     http_1.HTTP_PROVIDERS,
     Constants_1.Constants,
     LogService_1.LogService,
+    ErrorService_1.ErrorService,
     StorageService_1.StorageService,
+    AuthService_1.AuthService,
     HttpClient_1.HttpClient
 ]);
 //# sourceMappingURL=boot.js.map
---
+++ Angular2/src/Web/wwwroot/appScripts/domain/auth/AuthenticationDetails.js
@@ -0,0 +1,7 @@
+var AuthenticationDetails = (function () {
+    function AuthenticationDetails() {
+    }
+    return AuthenticationDetails;
+})();
+exports.AuthenticationDetails = AuthenticationDetails;
+//# sourceMappingURL=AuthenticationDetails.js.map
