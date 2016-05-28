2f143aff7ac62dbca6e62a11ffca88d718a4c646
adipa <adipa.achintha@gmail.com>
2016-05-29 00:07:15 +1000

    speed optimizations

---
+++ Angular2/src/Web/scripts/system-config.js
@@ -0,0 +1,20 @@
+﻿System.config({
+    map: {
+        '@angular': 'libs/angular',
+        'rxjs':'libs/rxjs/Rx.js'
+    },
+    packages: {
+        '@angular/core': { main: 'core.umd.js' },
+        '@angular/http': { main: 'http.umd.js' },
+        '@angular/common': { main: 'common.umd.js' },
+        '@angular/compiler': { main: 'compiler.umd.js' },
+        '@angular/router': { main: 'router.umd.js' },
+        '@angular/platform-browser': { main: 'platform-browser.umd.js' },
+        '@angular/platform-browser-dynamic': { main: 'platform-browser-dynamic.umd.js' },
+        appScripts: {
+            main: 'app',
+            format: 'register',
+            defaultExtension: 'js'
+        }
+    }
+});
