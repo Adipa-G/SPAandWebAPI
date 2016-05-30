37f13ee81f3b787cc2c79a7ed322ceec993ccce3
adipa <adipa.achintha@gmail.com>
2016-05-30 23:09:43 +1000

    Pagination and loading screen while loading the application

--- Angular2/src/Web/scripts/common/directives/Pagination.ts
+++ Angular2/src/Web/scripts/common/directives/Pagination.ts
@@ -5,12 +5,18 @@ import {OrderAndPage} from '../../domain/common/OrderAndPage';
 @Component({
     selector: '[pagination]',
     template: '<div [hidden]="totalPages <= 1" class="btn-group btn-group-sm" role="group">' +
-              '     <button type="button" class="btn btn-default" [disabled]="pageOptions.pageNumber === 1" (click)="pageClick(pageOptions.pageNumber - 1)">' +
-              '         &nbsp;<i class="fa fa-angle-double-left"></i>' +
+              '     <button type="button" class="btn btn-default" [disabled]="pageOptions.pageNumber === 1" ' +
+              '      (click)="pageClick(pageOptions.pageNumber - 1)">' +
+              '         &nbsp;<i class="fa fa-angle-left"></i>' +
               '     </button>' +
-              '     <button *ngFor="let number of [1,2,3,4,5]" type="button" class="btn btn-default">{{start + number}}</button>' +
-              '     <button type="button" class="btn btn-default" [disabled]="pageOptions.pageNumber === totalCount" (click)="pageClick(pageOptions.pageNumber + 1)">' +
-              '         <i class="fa fa-angle-double-right"></i>&nbsp;' +
+              '     <button *ngFor="let number of [0,1,2,3,4]" type="button" class="btn" ' +
+              '       [ngClass]="{\'btn-primary\': pageOptions.pageNumber === (start + number), \'btn-default\': pageOptions.pageNumber != (start + number)}"' +
+              '       [hidden]="(start + number) > totalPages"' +
+              '       (click)="pageClick(start + number)">{{start + number}}' +
+              '     </button>' +
+              '     <button type="button" class="btn btn-default" [disabled]="pageOptions.pageNumber === totalCount" ' +
+              '      (click)="pageClick(pageOptions.pageNumber + 1)">' +
+              '         <i class="fa fa-angle-right"></i>&nbsp;' +
               '     </button>' +
               '</div>'
 })
@@ -20,35 +26,30 @@ export class Pagination implements OnChanges {
     @Input('total-count') totalCount: number;
     @Output('page-changed') pageChanged = new EventEmitter<OrderAndPage>();
 
-    private start : number;
+    private start : number = 1;
     private totalPages: number;
 
-    public pageClick(pageNumber:number): void {
+    public pageClick(pageNumber: number): void {
+        if (pageNumber > this.start + 4 && this.start <= this.totalPages) {
+            this.start = this.start + 1;
+        } else if (pageNumber < this.start && this.start > 1) {
+            this.start = this.start - 1;
+        }
         this.pageOptions.pageNumber = pageNumber;
         this.setPages();
         this.pageChanged.emit(this.pageOptions);
     }
 
     ngOnChanges(changes) {
-        debugger;
         this.setPages();
     }
 
     private setPages(): void {
-        debugger;
-
         var pageSize = 1;
         if (this.pageOptions.pageSize > 0) {
             pageSize = this.pageOptions.pageSize;
         }
 
-        this.totalPages = this.totalCount / pageSize;
-
-        var start = this.pageOptions.pageNumber - 2;
-        if (start < 0) {
-            start = 0;
-        }
-
-        this.start = start;
+        this.totalPages = Math.ceil(this.totalCount / pageSize);
     }
 }
