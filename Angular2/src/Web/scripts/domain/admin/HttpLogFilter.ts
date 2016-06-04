ee0e6da88bff72f27041b5ddddc0a981472a9ca4
adipa <adipa.achintha@gmail.com>
2016-06-04 20:57:37 +1000

    log viewers

---
+++ Angular2/src/Web/scripts/domain/admin/HttpLogFilter.ts
@@ -0,0 +1,10 @@
+﻿import {OrderAndPage} from '../common/OrderAndPage';
+
+export class HttpLogFilter extends OrderAndPage {
+    logLevel: string;
+    trackId: string;
+    fromDateLocal : string;
+    fromDate: string;
+    toDateLocal: string;
+    toDate : string;
+}
