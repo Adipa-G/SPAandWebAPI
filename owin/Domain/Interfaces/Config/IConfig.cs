ee4f7e0d4a87aefb5b6d5c929b07299bd13b49a1
adipa <adipa.achintha@gmail.com>
2016-06-09 23:01:00 +1000

    owin subtree

---
+++ owin/Domain/Interfaces/Config/IConfig.cs
@@ -0,0 +1,11 @@
+﻿using Domain.Enum;
+
+namespace Domain.Interfaces.Config
+{
+    public interface IConfig
+    {
+        LogLevel LogLevelGeneral { get; set; }
+        bool LogRequests { get; set; }
+        bool LogSqlStatements { get; set; }
+    }
+}
