d48fdaccc5cab68cee164929491c4ea085372993
adipa <adipa.achintha@gmail.com>
2016-06-09 23:01:00 +1000

    owin subtree

---
+++ owin/Infrastructure.Test/Repositories/ConfigRepositoryTest.cs
@@ -0,0 +1,68 @@
+﻿using Domain.Entities;
+using Infrastructure.Repositories;
+using NUnit.Framework;
+
+namespace Infrastructure.Test.Repositories
+{
+    [TestFixture]
+    public class ConfigRepositoryTest : RepositoryTestBase
+    {
+        [TestFixtureSetUp]
+        protected override void OneTimeSetUp()
+        {
+            base.OneTimeSetUp();
+        }
+
+        [TestFixtureTearDown]
+        protected override void OneTimeTearDown()
+        {
+            base.OneTimeTearDown();
+        }
+
+        [SetUp]
+        protected override void SetUp()
+        {
+            base.SetUp();
+        }
+
+        [TearDown]
+        public override void TearDown()
+        {
+            base.TearDown();
+        }
+
+        [Test]
+        public void GivenSettingValue_WhenGetSettingValue_ThenReturnValue()
+        {
+            Session.Save(new ConfigSetting() {ConfigKey = "abc", ConfigValue = "def"});
+            FlushAndClear();
+
+            var sut = new ConfigRepository(Session);
+            var value = sut.GetSettingValue("abc", "pqr");
+
+            Assert.AreEqual("def",value);
+        }
+
+        [Test]
+        public void GivenNoSettingValue_WhenGetSettingValue_ThenWriteDefaultValue()
+        {
+            var sut = new ConfigRepository(Session);
+            sut.GetSettingValue("abc", "pqr");
+            FlushAndClear();
+
+            var value = sut.GetSettingValue("abc", "lmn");
+            Assert.AreEqual("pqr", value);
+        }
+
+        [Test]
+        public void GivenNoSettingValue_WhenSetSettingValue_ThenSetValue()
+        {
+            var sut = new ConfigRepository(Session);
+            sut.SetSettingValue("abc", "pqr");
+            FlushAndClear();
+
+            var value = sut.GetSettingValue("abc", "lmn");
+            Assert.AreEqual("pqr", value);
+        }
+    }
+}
---
+++ owin/Web/web-src/app/directives/utcView.js
@@ -0,0 +1,17 @@
+﻿'use strict';
+
+(function () {
+    var app = angular.module('AngularAuthApp');
+    app.directive('utcView', function () {
+        return {
+            restrict: 'A',
+            link: function (scope, elem, attrs) {
+                attrs.$observe('utcView', function (value) {
+                    var localTime = moment.utc(value).toDate();
+                    localTime = moment(localTime).format('YYYY-MM-DD HH:mm:ss');
+                    elem.text(localTime);
+                });
+            }
+        };
+    });
+})();
