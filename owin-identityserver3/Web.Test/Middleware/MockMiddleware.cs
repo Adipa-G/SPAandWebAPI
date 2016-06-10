5cabed5849e480085611c3656fb347a1a20f9e78
adipa <adipa.achintha@gmail.com>
2016-06-10 22:56:24 +1000

    owin-identityserver3 subtree

---
+++ owin-identityserver3/Web.Test/Middleware/MockMiddleware.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+
+namespace Web.Test.Middleware
+{
+    public class MockMiddleware : OwinMiddleware
+    {
+        public MockMiddleware() : base(null)
+        {
+            InvokeCount = 0;
+        }
+
+        public bool ThrowException { get; set; }
+
+        public int InvokeCount { get; set; }
+
+        public override Task Invoke(IOwinContext context)
+        {
+            if (ThrowException)
+            {
+                throw new Exception();
+            }
+            InvokeCount ++;
+            return Task.FromResult(0);
+        }
+    }
+}
