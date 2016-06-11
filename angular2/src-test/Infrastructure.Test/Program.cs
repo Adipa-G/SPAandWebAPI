dbcc5f490a592ee4f8b355b63f7d28cb476e7028
adipa <adipa.achintha@gmail.com>
2016-06-11 14:19:53 +1000

    merge angular2 updates to master

--- angular2/src-test/Infrastructure.Test/Program.cs
+++ angular2/src-test/Infrastructure.Test/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Threading.Tasks;
 using NUnitLite;
 
 namespace Infrastructure.Test
@@ -11,11 +7,7 @@ namespace Infrastructure.Test
     {
         public static void Main(string[] args)
         {
-#if DNX451
             new AutoRun().Execute(args);
-#else
-        new AutoRun().Execute(typeof(Program).GetTypeInfo().Assembly, Console.Out, Console.In, args);
-#endif
             Console.ReadLine();
         }
     }
--- merge-all.bat
+++ merge-all.bat
@@ -4,3 +4,6 @@ echo git read-tree --prefix=owin-identityserver3/ -u owin-identityserver3
 echo git read-tree --prefix=mvc6/ -u mvc6
 
 git merge --squash -s subtree --no-commit angular2
+git merge --squash -s subtree --no-commit owin
+git merge --squash -s subtree --no-commit owin-identityserver3
+git merge --squash -s subtree --no-commit mvc6
