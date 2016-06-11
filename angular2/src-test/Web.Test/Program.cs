5aeac2eab3c290666a29aba28c88753a4231cddf
adipa <adipa.achintha@gmail.com>
2016-06-11 14:19:53 +1000

    merge angular2 updates to master

--- angular2/src-test/Web.Test/Program.cs
+++ angular2/src-test/Web.Test/Program.cs
@@ -7,11 +7,7 @@ namespace Web.Test
     {
         public static void Main(string[] args)
         {
-#if DNX451
             new AutoRun().Execute(args);
-#else
-        new AutoRun().Execute(typeof(Program).GetTypeInfo().Assembly, Console.Out, Console.In, args);
-#endif
             Console.ReadLine();
         }
     }
