9c2c727bc597ecf25ff46d79a8e34354e2955670
adipa <adipa.achintha@gmail.com>
2016-06-11 14:18:35 +1000

    mvc6 branch

---
+++ mvc6/src-test/Web.Test/web/app/directives/tablePagerTest.js
@@ -0,0 +1,79 @@
+﻿'use strict';
+
+describe('table pager test', function () {
+    var $compile;
+    var $rootScope;
+
+    beforeEach(module('AngularAuthApp'));
+
+    beforeEach(inject(function (_$compile_, _$rootScope_) {
+        $compile = _$compile_;
+        $rootScope = _$rootScope_;
+    }));
+
+    it('Set pagination styles', function () {
+        var element = $compile("<div table-pager=\"sortAndPage\"></div>")($rootScope);
+        $rootScope.$digest();
+
+        var scope = element.scope();
+        scope.sortData = { OrderField: 'UserName', OrderDirection: 'Asc' };
+        scope.$digest();
+
+        expect(element.html()).toContain("pagination-sm");
+    });
+
+    it('Set show condition', function () {
+        var element = $compile("<div table-pager=\"sortAndPage\"></div>")($rootScope);
+        $rootScope.$digest();
+
+        var scope = element.scope();
+        scope.sortData = { OrderField: 'UserName', OrderDirection: 'Asc' };
+        scope.$digest();
+
+        expect(element.html()).toContain("sortAndPage.totalCount&gt;sortAndPage.PageSize");
+    });
+
+    it('Set total items count', function () {
+        var element = $compile("<div table-pager=\"sortAndPage\"></div>")($rootScope);
+        $rootScope.$digest();
+
+        var scope = element.scope();
+        scope.sortData = { OrderField: 'UserName', OrderDirection: 'Asc' };
+        scope.$digest();
+
+        expect(element.html()).toContain("total-items=\"sortAndPage.totalCount\"");
+    });
+
+    it('Set model', function () {
+        var element = $compile("<div table-pager=\"sortAndPage\"></div>")($rootScope);
+        $rootScope.$digest();
+
+        var scope = element.scope();
+        scope.sortData = { OrderField: 'UserName', OrderDirection: 'Asc' };
+        scope.$digest();
+
+        expect(element.html()).toContain("ng-model=\"sortAndPage.PageNumber\"");
+    });
+
+    it('Set items per page', function () {
+        var element = $compile("<div table-pager=\"sortAndPage\"></div>")($rootScope);
+        $rootScope.$digest();
+
+        var scope = element.scope();
+        scope.sortData = { OrderField: 'UserName', OrderDirection: 'Asc' };
+        scope.$digest();
+
+        expect(element.html()).toContain("items-per-page=\"sortAndPage.PageSize\"");
+    });
+
+    it('Set change event', function () {
+        var element = $compile("<div table-pager=\"sortAndPage\"></div>")($rootScope);
+        $rootScope.$digest();
+
+        var scope = element.scope();
+        scope.sortData = { OrderField: 'UserName', OrderDirection: 'Asc' };
+        scope.$digest();
+
+        expect(element.html()).toContain("ng-change=\"sortAndPage.onChange()\"");
+    });
+});
