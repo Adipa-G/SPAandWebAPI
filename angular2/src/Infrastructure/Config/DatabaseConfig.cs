f001051d9907a57c0479b618419a90759edc3ae1
adipa <adipa.achintha@gmail.com>
2016-07-13 00:00:27 +1000

    update the master

---
+++ angular2/src/Infrastructure/Config/DatabaseConfig.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+using Domain.Interfaces.Config;
+using Microsoft.Extensions.Configuration;
+
+namespace Infrastructure.Config
+{
+    public class DatabaseConfig : IDatabaseConfig
+    {
+        private IConfiguration _configuration;
+
+        public DatabaseConfig(string file)
+        {
+            _configuration = new ConfigurationBuilder().AddJsonFile(file).Build();
+        }
+
+        public string Server => _configuration["Server"];
+        public string Database => _configuration["Database"];
+        public string Username => _configuration["Username"];
+        public string Password => _configuration["Password"];
+        public string ConnectionString => _configuration["ConnectionString"];
+    }
+}
---
+++ angular2/src/Web/scripts/app.ts
@@ -0,0 +1,55 @@
+﻿import {Component, OnInit} from '@angular/core';
+import { Router,Routes, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from '@angular/router';
+import 'rxjs/add/operator/map';
+
+import {MenuComponent} from './common/components/MenuComponent';
+import {HomeComponent} from './common/components/HomeComponent';
+import {ErrorComponent} from './common/components/ErrorComponent';
+import {LoginComponent} from './common/components/LoginComponent';
+import {RegisterComponent} from './common/components/RegisterComponent';
+
+import {UserListComponent} from './admin/components/UserListComponent';
+import {HttpLogComponent} from './admin/components/HttpLogComponent';
+import {LogMessagesComponent} from './admin/components/LogMessagesComponent';
+
+@Component({
+    selector: 'angular-auth-app',
+    templateUrl: './templates/app.html',
+    directives: [ROUTER_DIRECTIVES, MenuComponent, ErrorComponent, HomeComponent, LoginComponent, UserListComponent],
+    providers: [ROUTER_PROVIDERS]
+})
+
+@Routes([
+    {
+        path: '/home',
+        component: HomeComponent
+    },
+    {
+        path: '/login',
+        component: LoginComponent
+    },
+    {
+        path: '/register',
+        component: RegisterComponent
+    },
+    {
+        path: '/userList',
+        component: UserListComponent
+    },
+    {
+        path: '/httpLog',
+        component: HttpLogComponent
+    },
+    {
+        path: '/logMessages',
+        component: LogMessagesComponent
+    }
+])
+
+export class AppComponent implements OnInit {
+    constructor(private router: Router) { }
+
+    ngOnInit() {
+        this.router.navigate(['/home']);
+    }   
+}
