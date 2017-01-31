59bf812d7a7b330f0e9baaa85e3f344e7f0494b3
adipa <adipa.achintha@gmail.com>
2017-01-31 21:56:12 +1100

    updating master

---
+++ mvc6/src/Web/web-src/app/services/authService.js
@@ -0,0 +1,65 @@
+﻿'use strict';
+
+(function() {
+    var authServiceFactory = function ($http, $q, localStorageService, $log, ngAuthSettings) {
+        var serviceBase = ngAuthSettings.apiServiceBaseUri;
+     
+        var authentication = {
+            isAuth: false,
+            userName: ""
+        };
+
+        var logOut = function() {
+            localStorageService.remove('authorizationData');
+            authentication.isAuth = false;
+            authentication.userName = "";
+        };
+
+        var saveRegistration = function(registration) {
+            logOut();
+            return $http.post(serviceBase + 'api/account/register', registration).catch(function(err) {
+                $log.error(err);
+            });
+        };
+
+        var login = function (loginData) {
+            var deferred = $q.defer();
+
+            var data = "grant_type=password&username=" + loginData.userName + "&password=" + loginData.password + "&client_id=default&client_secret=no-secret&scope=all";
+            var headers = { headers: { 'Content-Type': 'application/x-www-form-urlencoded' } };
+
+            $http.post(serviceBase + 'connect/token', data, headers)
+            .then(function (response) {
+                localStorageService.set('authorizationData', { token: response.data.access_token, userName: loginData.userName });
+                authentication.isAuth = true;
+                authentication.userName = loginData.userName;
+                deferred.resolve(authentication);
+            }).catch(function (err) {
+                $log.error(err);
+                logOut();
+                deferred.reject(err.data);
+            });
+
+            return deferred.promise;
+        };
+
+        var fillAuthData = function() {
+            var authData = localStorageService.get('authorizationData');
+            if (authData) {
+                authentication.isAuth = true;
+                authentication.userName = authData.userName;
+            }
+        };
+
+        return {
+            saveRegistration : saveRegistration,
+            login : login,
+            logOut : logOut,
+            fillAuthData : fillAuthData,
+            authentication : authentication
+        };
+    };
+    
+    var app = angular.module('AngularAuthApp');
+    app.factory('authService', ['$http', '$q', 'localStorageService', '$log', 'ngAuthSettings', authServiceFactory]);
+})();
