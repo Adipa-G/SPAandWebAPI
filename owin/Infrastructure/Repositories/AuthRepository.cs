319b932e0da0286f40a0f5b71df7e4e17d67e48d
adipa <adipa.achintha@gmail.com>
2017-01-31 21:56:53 +1100

    updating master

---
+++ owin/Infrastructure/Repositories/AuthRepository.cs
@@ -0,0 +1,93 @@
+﻿using System.Linq;
+using Domain.Interfaces.Repositories;
+using Domain.Models;
+using Domain.Models.Auth;
+using Microsoft.AspNet.Identity;
+using NHibernate;
+using NHibernate.AspNet.Identity;
+using NHibernate.Criterion;
+
+namespace Infrastructure.Repositories
+{
+    public class AuthRepository : IAuthRepository
+    {
+        private ISession _session;
+        private UserManager<IdentityUser> _userManager;
+
+        public AuthRepository(ISession session,UserManager<IdentityUser> userManager)
+        {
+            _session = session;
+            _userManager = userManager;
+        }
+
+        public  IdentityResult RegisterUser(UserModel userModel)
+        {
+            IdentityUser user = new IdentityUser
+            {
+                UserName = userModel.UserName
+            };
+            var result = _userManager.Create(user, userModel.Password);
+            return result;
+        }
+
+        public IdentityUser FindUser(string userName, string password)
+        {
+            IdentityUser user = _userManager.Find(userName, password);
+            return user;
+        }
+        
+        public IdentityUser Find(UserLoginInfo loginInfo)
+        {
+            IdentityUser user = _userManager.Find(loginInfo);
+            return user;
+        }
+
+        public IdentityResult Create(IdentityUser user)
+        {
+            var result = _userManager.Create(user);
+            return result;
+        }
+
+        public  IdentityResult AddLogin(string userId, UserLoginInfo login)
+        {
+            var result = _userManager.AddLogin(userId, login);
+            return result;
+        }
+
+        public ListResult<UserListItemModel> List(ListRequest request)
+        {
+            var query = _session.QueryOver<IdentityUser>();
+
+            if (request.OrderDirection == SortDirection.Asc)
+            {
+                query = query.OrderBy(Projections.Property(request.OrderField)).Asc;
+            }
+            else if (request.OrderDirection == SortDirection.Desc)
+            {
+                query = query.OrderBy(Projections.Property(request.OrderField)).Desc;
+            }
+
+            var totalCount = query.RowCount();
+            var results =
+                query.Skip((request.PageNumber - 1) * request.PageSize)
+                    .Take(request.PageSize)
+                    .List()
+                    .Select(r => new UserListItemModel() { UserName = r.UserName })
+                    .ToList();
+
+            return new ListResult<UserListItemModel>()
+            {
+                PageNumber = request.PageNumber,
+                PageSize = request.PageSize,
+                Results = results,
+                TotalCount = totalCount
+            };
+        }
+
+        public void Delete(string userName)
+        {
+            var identityUser = _userManager.FindByName(userName);
+            _userManager.Delete(identityUser);
+        }
+    }
+}
---
+++ owin/Web/web-src/app/controllers/logHttpController.js
@@ -0,0 +1,35 @@
+﻿'use strict';
+
+(function () {
+    var logHttpController = function ($scope, $q, logService, utilsService) {
+        $scope.logs = [];
+        $scope.showFromCalender = false;
+        $scope.showToCalender = false;
+        $scope.filter = { OrderField: 'CalledOn', OrderDirection: 'Desc', PageNumber: 1, PageSize: 100 };
+
+        $scope.toggleFromCalender = function() {
+            $scope.showFromCalender = !$scope.showFromCalender;
+        };
+
+        $scope.toggleToCalender = function () {
+            $scope.showToCalender = !$scope.showToCalender;
+        };
+
+        var populateData = function () {
+            $scope.filter.FromDate = utilsService.dateToUtcFormat($scope.filter._FromDate);
+            $scope.filter.ToDate = utilsService.dateToUtcFormat($scope.filter._ToDate);
+
+            $q.all([logService.getLogHttp($scope.filter).then(function (results) {
+                $scope.logs = results.data.results;
+                $scope.filter.totalCount = results.data.totalCount;
+            }), logService.getLogLevels().then(function(results) {
+                $scope.logLevels = results.data;
+            })]);
+        };
+        populateData();
+        $scope.filter.onChange = populateData;
+    };
+
+    var app = angular.module('AngularAuthApp');
+    app.controller('logHttpController', ['$scope', '$q', 'logService', 'utilsService', logHttpController]);
+})();
