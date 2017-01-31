6d8e3da210597e96b7d352343f4a8592d51c647b
adipa <adipa.achintha@gmail.com>
2017-01-31 21:55:44 +1100

    updating master

---
+++ angular2/src/Web/Middleware/ContentTypeProvider.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.AspNetCore.StaticFiles;
+
+namespace Web.Middleware
+{
+    public class ContentTypeProvider : IContentTypeProvider
+    {
+        private FileExtensionContentTypeProvider provider;
+
+        public ContentTypeProvider()
+        {
+            provider = new FileExtensionContentTypeProvider();
+        }
+
+        public bool TryGetContentType(string subpath, out string contentType)
+        {
+            subpath = DetectHtml5Routing(subpath);
+            return provider.TryGetContentType(subpath, out contentType);
+        }
+
+        private static string DetectHtml5Routing(string subpath)
+        {
+            var index = subpath.ToLower().IndexOf(".html/", StringComparison.Ordinal);
+            if (index > 0)
+            {
+                subpath = $"{subpath.Substring(0, index)}.html";
+            }
+            return subpath;
+        }
+    }
+}
