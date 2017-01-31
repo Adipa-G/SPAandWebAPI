97a88166de1c9d1e7ca1c7f7d653a2d48d97b13f
adipa <adipa.achintha@gmail.com>
2017-01-31 21:54:54 +1100

    # Conflicts: #	mvc6/src/Web/web-src/app/common-scripts.js

--- angular2/src/Domain/Entities/User.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Domain.Entities
-{
-    public class User
-    {
-        public virtual long UserId { get; set; }
-        public virtual string UserName { get; set; }
-        public virtual string Password { get; set; }
-    }
-}
