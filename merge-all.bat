58e283f9798414b7a6eb4e0747ff696ee5b218aa
adipa <adipa.achintha@gmail.com>
2017-05-11 23:50:45 +1000

    angular upgrade markdown fix

--- merge-all.bat
+++ merge-all.bat
@@ -1,9 +1,9 @@
-echo git read-tree --prefix=angular2/ -u angular2
+echo git read-tree --prefix=angular4/ -u angular4
 echo git read-tree --prefix=owin/ -u owin
 echo git read-tree --prefix=owin-identityserver3/ -u owin-identityserver3
 echo git read-tree --prefix=mvc6/ -u mvc6
 
-git merge --squash -s subtree --no-commit angular2
+git merge --squash -s subtree --no-commit angular4
 git merge --squash -s subtree --no-commit owin
 git merge --squash -s subtree --no-commit owin-identityserver3
 git merge --squash -s subtree --no-commit mvc6
