adf2b7ee65058eeeed10c7a7f468648ec2f9d07f
adipa-g <adipa.achntha@gmail.com>
2018-10-24 00:03:59 +1100

    remove angular4

--- angular4/src/Domain/Entities/ConfigSetting.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Domain.Entities
-{
-    public class ConfigSetting
-    {
-        public virtual long Id { get; set; }
-        public virtual string ConfigKey { get; set; }
-        public virtual string ConfigValue { get; set; }
-    }
-}
