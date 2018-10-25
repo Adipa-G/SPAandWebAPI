87a5e6d566ac646212d5cfa319014f8faebd7565
adipa <adipa.achintha@gmail.com>
2018-10-25 23:33:30 +1100

    mvc6 rename

--- merge-all.bat
+++ merge-all.bat
@@ -3,11 +3,11 @@ echo make sure branches are available in local repo before running read-tree com
 git read-tree --prefix=angular/ -u angular
 echo git read-tree --prefix=owin/ -u owin
 echo git read-tree --prefix=owin-identityserver3/ -u owin-identityserver3
-echo git read-tree --prefix=mvc6/ -u mvc6
-echo git read-tree --prefix=react/ -u react
+git read-tree --prefix=netcore/ -u netcore
+git read-tree --prefix=react/ -u react
 
 git merge --squash -s subtree --no-commit angular
 echo git merge --squash -s subtree --no-commit owin
 echo git merge --squash -s subtree --no-commit owin-identityserver3
-echo git merge --squash -s subtree --no-commit mvc6
-echo git merge --squash -s subtree --no-commit react
+git merge --squash -s subtree --no-commit netcore
+git merge --squash -s subtree --no-commit react
