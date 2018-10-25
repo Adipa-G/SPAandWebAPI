7c7ca0fb5a03f06b83200f9dd6118b8be8dce383
adipa <adipa.achntha@gmail.com>
2018-10-25 23:35:03 +1100

    # Conflicts: #	react/build.bat #	react/src-test/Infrastructure.Test/Infrastructure.Test.csproj #	react/src-test/Web.Test/Web.Test.csproj #	react/src/Domain/Domain.csproj #	react/src/Infrastructure/Infrastructure.csproj #	react/src/Web/Middleware/RequestResponseLog.cs #	react/src/Web/Startup.cs #	react/src/Web/Web.csproj

--- react/build.bat
+++ react/build.bat
@@ -1,22 +1,18 @@
-nuget\nuget.exe restore Mvc.sln
+dotnet restore
 if %errorlevel% neq 0 exit /b %errorlevel%
-"C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" Mvc.sln
+dotnet build
 if %errorlevel% neq 0 exit /b %errorlevel%
-dotnet build src-test\Infrastructure.Test\Infrastructure.Test.csproj
+dotnet src-test\Infrastructure.Test\bin\Debug\netcoreapp2.1\Infrastructure.Test.dll
 if %errorlevel% neq 0 exit /b %errorlevel%
-dotnet src-test\Infrastructure.Test\bin\Debug\netcoreapp2\Infrastructure.Test.dll
-if %errorlevel% neq 0 exit /b %errorlevel%
-dotnet build src-test\Web.Test\Web.Test.csproj
-if %errorlevel% neq 0 exit /b %errorlevel%
-dotnet src-test\Web.Test\bin\Debug\netcoreapp2\Web.Test.dll
+dotnet src-test\Web.Test\bin\Debug\netcoreapp2.1\Web.Test.dll
 pushd src\Web
-call npm install -g --cache-min 999999 webpack webpack-cli jest
+call npm install -g --prefer-offline webpack webpack-cli jest
 if %errorlevel% neq 0 exit /b %errorlevel%
 call npm link webpack
 if %errorlevel% neq 0 exit /b %errorlevel%
-call npm install --cache-min 999999 gulp
+call npm install --prefer-offline gulp
 if %errorlevel% neq 0 exit /b %errorlevel%
-call npm install --cache-min 999999
+call npm install --prefer-offline
 if %errorlevel% neq 0 exit /b %errorlevel%
 call gulp copy-libs
 if %errorlevel% neq 0 exit /b %errorlevel%
@@ -32,6 +28,6 @@ popd
 echo ==================== DONE =======================
 echo ==================== STARTING APP =======================
 pushd src\Web
-dotnet bin\Debug\netcoreapp2\Web.dll
+dotnet bin\Debug\netcoreapp2.1\Web.dll
 if %errorlevel% neq 0 exit /b %errorlevel%
 popd
