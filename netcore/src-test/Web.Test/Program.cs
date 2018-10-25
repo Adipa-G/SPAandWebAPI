07aa44e68de41aa3d248bbdfd2431b7eaa589218
adipa <adipa.achntha@gmail.com>
2018-10-25 23:33:30 +1100

    mvc6 rename

---
+++ netcore/src-test/Web.Test/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using NUnitLite;
+
+namespace Web.Test
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            new AutoRun().Execute(args);
+        }
+    }
+}
