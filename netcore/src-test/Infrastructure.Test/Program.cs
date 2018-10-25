26eafd055f47758fb658d50ef5014df2e32510d2
adipa-g <adipa.achintha@gmail.com>
2018-10-25 23:33:30 +1100

    mvc6 rename

---
+++ netcore/src-test/Infrastructure.Test/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using NUnitLite;
+
+namespace Infrastructure.Test
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            new AutoRun().Execute(args);
+        }
+    }
+}
