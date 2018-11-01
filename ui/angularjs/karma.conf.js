02d89889c638d84f1dd1fd0e2da1a76e0c0f6e20
adipa <adipa.achntha@gmail.com>
2018-11-01 18:26:08 +1100

    + ui/angularjs

---
+++ ui/angularjs/karma.conf.js
@@ -0,0 +1,35 @@
+﻿module.exports = function (config) {
+    config.set({
+        basePath: '',
+        frameworks: ['jasmine'],
+        files: [
+          './dist/lib/jquery-2.2.4.js',
+          './dist/lib/bootstrap.js',
+          './dist/lib/vkbeautify.js',
+          './dist/lib/highlight.min.js',
+          './dist/lib/moment.js',
+          './dist/lib/angular.js',
+          './dist/lib/angular-route.js',
+          './dist/lib/angular-local-storage.js',
+          './dist/lib/loading-bar.js',
+          './dist/lib/ui-bootstrap-tpls-0.14.3.js',
+          './dist/lib/ngFormValidator.1.4.1.0.js',
+          './dist/app/app.js',
+          './web-test/lib/angular-mocks.js',
+          './web-test/app/spec_helper.js',
+          './web-test/app/templates.js',
+          './web-test/app/**/*Test.js'
+        ],
+        exclude: [
+        ],
+        preprocessors: {},
+        reporters: ['dots'],
+        port: 9876,
+        colors: true,
+        logLevel: config.LOG_INFO,
+        autoWatch: true,
+        plugins : ['karma-jasmine', 'karma-phantomjs-launcher'],
+        browsers: ['PhantomJS'],
+        singleRun: false
+    });
+};
