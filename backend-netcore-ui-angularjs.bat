cc78ecf08a7bcba57ece786ae4a87fbec03272f9
adipa-g <adipa.achintha@gmail.com>
2018-11-01 18:23:42 +1100

    + batch scripts

---
+++ backend-netcore-ui-angularjs.bat
@@ -0,0 +1,20 @@
+echo ==================== UI =======================
+pushd ui\angularjs
+call build.bat
+popd
+echo ==================== Backend =======================
+pushd backend\netcore
+call build.bat
+popd
+rmdir /s /q backend\netcore\src\Web\app
+if %errorlevel% neq 0 exit /b %errorlevel%
+mkdir backend\netcore\src\Web\app
+if %errorlevel% neq 0 exit /b %errorlevel%
+xcopy ui\angularjs\dist backend\netcore\src\Web\app /s /e
+if %errorlevel% neq 0 exit /b %errorlevel%
+echo ==================== DONE =======================
+echo ==================== STARTING APP =======================
+pushd backend\netcore\src\Web
+dotnet bin\Debug\netcoreapp2.1\Web.dll
+if %errorlevel% neq 0 exit /b %errorlevel%
+popd
