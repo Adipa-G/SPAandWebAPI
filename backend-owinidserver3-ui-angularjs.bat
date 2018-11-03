e02fd5613d66c68de165ce8a2bf9759655e7893a
adipa-g <adipa.achintha@gmail.com>
2018-11-03 22:11:14 +1100

    + owin-idserver3

--- backend-owinidserver3-ui-angularjs.bat
+++ backend-owinidserver3-ui-angularjs.bat
@@ -3,18 +3,18 @@ pushd ui\angularjs
 call build.bat
 popd
 echo ==================== Backend =======================
-pushd backend\owin
+pushd backend\owin-identityserver3
 call build.bat
 popd
-rmdir /s /q backend\owin\Web\www
+rmdir /s /q backend\owin-identityserver3\Web\www
 if %errorlevel% neq 0 exit /b %errorlevel%
-mkdir backend\owin\Web\www
+mkdir backend\owin-identityserver3\Web\www
 if %errorlevel% neq 0 exit /b %errorlevel%
-xcopy ui\angularjs\dist backend\owin\Web\www /s /e
+xcopy ui\angularjs\dist backend\owin-identityserver3\Web\www /s /e
 if %errorlevel% neq 0 exit /b %errorlevel%
 echo ==================== DONE =======================
 echo ==================== STARTING APP =======================
-pushd backend\owin\Web
-"C:\Program Files\IIS Express\iisexpress.exe" /port:5000 /path:"D:\GitHub\SPAandWebAPI\backend\owin\Web"
+pushd backend\owin-identityserver3\Web
+"C:\Program Files\IIS Express\iisexpress.exe" /port:5000 /path:"D:\GitHub\SPAandWebAPI\backend\owin-identityserver3\Web"
 if %errorlevel% neq 0 exit /b %errorlevel%
 popd
---
+++ backend/owin-identityserver3/Domain/Interfaces/Config/IDatabaseConfig.cs
@@ -0,0 +1,11 @@
+﻿namespace Domain.Interfaces.Config
+{
+    public interface IDatabaseConfig
+    {
+        string Server { get; }
+        string Database { get; }
+        string Username { get; }
+        string Password { get; }
+        string ConnectionString { get; }
+    }
+}
