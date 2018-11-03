bdaae414bd3f0d123e64e47076eb4b63b0bf17b4
adipa <adipa.achintha@gmail.com>
2018-11-03 22:11:14 +1100

    + owin-idserver3

---
+++ backend/owin-identityserver3/build.bat
@@ -0,0 +1,9 @@
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\Common7\Tools\VsDevCmd.bat"
+.\.nuget\nuget.exe restore .\OwinIdServer3.sln
+if %errorlevel% neq 0 exit /b %errorlevel%
+MSBuild .\OwinIdServer3.sln
+if %errorlevel% neq 0 exit /b %errorlevel%
+VSTest.Console.exe "Infrastructure.Test\bin\Debug\Infrastructure.Test.dll" /TestAdapterPath:"Infrastructure.Test\bin\Debug\"
+if %errorlevel% neq 0 exit /b %errorlevel%
+VSTest.Console.exe "Web.Test\bin\Debug\Web.Test.dll" /TestAdapterPath:"Web.Test\bin\Debug\"
+echo if %errorlevel% neq 0 exit /b %errorlevel%
