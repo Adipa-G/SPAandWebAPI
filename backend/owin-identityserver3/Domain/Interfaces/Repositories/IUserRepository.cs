911c46bc3d3352660bd9a6e1452c0e7d9f6ac2d5
adipa-g <adipa.achntha@gmail.com>
2018-11-03 22:11:14 +1100

    + owin-idserver3

---
+++ backend/owin-identityserver3/Domain/Interfaces/Repositories/IUserRepository.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Models;
+using Domain.Models.Auth;
+
+namespace Domain.Interfaces.Repositories
+{
+    public interface IUserRepository
+    {
+        UserModel RegisterUser(UserModel userModel);
+        UserModel FindUser(string userName, string password);
+        ListResult<UserListItemModel> List(ListRequest request);
+        void Delete(string userName);
+    }
+}
