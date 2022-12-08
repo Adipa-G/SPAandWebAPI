7e94814177a9774ff31da77d160d77d05c61191b
adipa <adipa.achintha@gmail.com>
2022-12-08 14:35:05 +1100

    test build

--- backend/owin-identityserver3/build.bat
+++ backend/owin-identityserver3/build.bat
@@ -1,9 +1,9 @@
-call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\Common7\Tools\VsDevCmd.bat"
+call "C:\Program Files\Microsoft Visual Studio\2022\Professional\Common7\Tools\VsDevCmd.bat"
 .\.nuget\nuget.exe restore .\OwinIdServer3.sln
 if %errorlevel% neq 0 exit /b %errorlevel%
 MSBuild .\OwinIdServer3.sln
 if %errorlevel% neq 0 exit /b %errorlevel%
-VSTest.Console.exe "Infrastructure.Test\bin\Debug\Infrastructure.Test.dll" /TestAdapterPath:"Infrastructure.Test\bin\Debug\"
+VSTest.Console.exe "Infrastructure.Test\bin\Debug\Infrastructure.Test.dll" /Logger:trx;LogFileName="Infrastructure.Test.trx" /TestAdapterPath:"Infrastructure.Test\bin\Debug\"
 if %errorlevel% neq 0 exit /b %errorlevel%
-VSTest.Console.exe "Web.Test\bin\Debug\Web.Test.dll" /TestAdapterPath:"Web.Test\bin\Debug\"
+VSTest.Console.exe "Web.Test\bin\Debug\Web.Test.dll" /Logger:trx;LogFileName="Web.Test.trx" /TestAdapterPath:"Web.Test\bin\Debug\" 
 echo if %errorlevel% neq 0 exit /b %errorlevel%
