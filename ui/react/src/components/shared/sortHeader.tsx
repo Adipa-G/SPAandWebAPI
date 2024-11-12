66afcd8a391001616928320153114ae8bf0f9ee2
adipa <adipa.achintha@gmail.com>
2024-11-12 23:26:02 +1100

    test the build

--- ui/react/src/components/shared/sortHeader.tsx
+++ ui/react/src/components/shared/sortHeader.tsx
@@ -34,7 +34,7 @@ const SortHeader = (props: SortHeaderProps) => {
             props.orderData.orderDirection = orderDirection;
             props.orderChanged();
         }
-    }, [orderDirection, orderField, props]);
+    }, [orderDirection, orderField]);
 
     const toggleOrder = () => {
         if (!props.orderData) {
