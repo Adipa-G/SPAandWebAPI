89fe118f33803311b13f8136979f590d1eb516b7
adipa <adipa.achintha@gmail.com>
2025-08-18 01:14:58 +1000

    update node version

--- ui/angular/app/src/app/app.component.ts
+++ ui/angular/app/src/app/app.component.ts
@@ -22,7 +22,6 @@ import { ServerLogService } from './admin/services/serverLogService';
 
 export class AppComponent implements OnInit {
     constructor(private router: Router) { }
-
     ngOnInit() {
         this.router.navigate(['/home']);
     }
