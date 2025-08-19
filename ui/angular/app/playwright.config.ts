62f31234ba1a9738a1c4cd569eb51ade8ca88549
adipa <adipa.achintha@gmail.com>
2025-08-19 23:03:04 +1000

    playwright e2e tests

--- ui/angular/app/playwright.config.ts
+++ ui/angular/app/playwright.config.ts
@@ -37,7 +37,6 @@ export default defineConfig({
       use: { ...devices['Desktop Chrome'], headless: true },
     }
 
-        
     /* Test against mobile viewports. */
     // {
     //   name: 'Mobile Chrome',
