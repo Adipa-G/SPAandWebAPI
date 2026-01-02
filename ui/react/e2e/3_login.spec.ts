88b0178ae64d238966db1a637aaba2e44da8e8b0
adipa <adipa.achintha@gmail.com>
2026-01-02 14:23:05 +1100

    update react

---
+++ ui/react/e2e/3_login.spec.ts
@@ -0,0 +1,23 @@
+import { expect, test } from '@playwright/test';
+import { registerUser } from './utils/auth-helpers';
+
+const userName = `testuser_${Date.now()}`;
+const password = 'Welcome@123';
+
+test('login user', async ({ page }) => {
+  await page.goto('/');
+
+  await registerUser(page, userName, password);
+  
+  await page.click('a:has-text("Login")');
+
+  await page.waitForURL('/#/login');
+  await page.waitForSelector('form', { state: 'visible' });
+
+  await page.locator('[name="loginUserName"]').fill(userName);
+  await page.locator('[name="loginPassword"]').fill(password);
+
+  await page.click('button:has-text("Login")');
+
+  await page.waitForURL('/#/users');
+});
---
+++ ui/react/e2e/4_userList.spec.ts
@@ -0,0 +1,17 @@
+import { expect, test } from '@playwright/test';
+import { loginUser, registerUser } from './utils/auth-helpers';
+
+const userName = `testuser_${Date.now()}`;
+const password = 'Welcome@123';
+
+test('shows users list', async ({ page }) => {
+  await page.goto('/');
+
+  await registerUser(page, userName, password);
+  await loginUser(page, userName, password);
+
+  await page.waitForURL('/#/users');
+  
+  var htmlContent = await page.content();
+  expect(htmlContent).toContain(userName);
+});
